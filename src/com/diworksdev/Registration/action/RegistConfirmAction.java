64e5ab0e8c9712167b7b1bf5db51cfa5a4cb178c
TakayukiH4R4 <sthaikgaoytuok4i@gmail.com>
2026-08-03 16:15:17 +0900

    registConfirmActionの作成

---
+++ src/com/diworksdev/Registration/action/RegistConfirmAction.java
@@ -0,0 +1,10 @@
+package com.diworksdev.Registration.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class RegistConfirmAction extends ActionSupport {
+	public String execute() {
+		return SUCCESS;
+	}
+
+}
